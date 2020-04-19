0080645374a8dbb46b7bfb3b64e157f6735c1689
radix <xidar007@gmail.com>
2020-04-20 03:41:12 +0530

    Initial Commit

---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# File : guessinggame.sh
+
+z=$(ls | wc -l ) 
+echo "How Many Files Are in The Current Directory ??? "
+echo "Please Enter Your Guess ::" 
+read numfiles
+echo "" 
+function guess { 
+echo "Please Enter Your Guess ::" 
+read numfiles
+echo ""
+}
+while [[ $numfiles -ne $z ]] 
+do
+if [[ $numfiles -gt $z ]] 
+then 
+	echo "Guess Too High , Please Try Again "
+	echo "" 
+        guess
+elif [[ $numfiles -lt $z ]]
+then
+	echo "Guess Too Low , Please Try Again"
+	echo ""
+ 	guess
+fi
+done
+	echo "Congratulations! $numfiles is the correct answer"
---
+++ makefile
@@ -0,0 +1,15 @@
+all : Readme.md
+
+Readme.md :
+	
+	echo  "#Guessing Game Using Bash " > Readme.md
+	echo  ""
+	echo  "###Date and Time at which make was run ::"  >> Readme.md
+	echo  ""
+	date >> Readme.md
+	echo  ""
+	echo  "####Number of lines of code contained in guessinggame.sh ::" >> Readme.md
+	wc -l guessinggame.sh  | egrep -o "[0-9]+" >> Readme.md
+
+clean :
+	rm Readme.md
